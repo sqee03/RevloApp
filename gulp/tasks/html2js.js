32e826f3bb851650a6abd5cec7d3e2aabd81954b
sqee03 <lubos.hradecky@gmail.com>
2020-02-06 11:19:41 +0100

    Fixed typo

--- gulp/tasks/html2js.js
+++ gulp/tasks/html2js.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
 
-gulp.task('html2js', function () {
+gulp.task('html2js', function() {
   return gulp.src([config.source.tpl.src, '!' + config.source.tpl.main])
     .pipe(templateCache('templates.js', {module: 'templateCache', standalone: true}))
     .pipe(rename({
